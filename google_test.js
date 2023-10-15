83929039856707d813d33410216c6e72fcb2e747
user <user@MacBook-Pro-user.local>
2023-10-15 15:06:37 +0300

    Fix ElementNotInteractableError

--- google_test.js
+++ google_test.js
@@ -12,10 +12,12 @@ driver.sleep(1000).then(() => {
   driver.findElement(By.name('q')).sendKeys(webdriver.Key.TAB)
 })
 
-driver.findElement(By.name('btnK')).click()
+// driver.findElement(By.name('btnK')).click()
+driver.findElement(By.xpath(`//div[@class='FPdoLc lJ9FBc']//input[@name='btnK']`)).click()
 
 driver.sleep(2000).then(() => {
   driver.getTitle().then((title) => {
+    // if (title === 'webdriver - Пошук Google') {
     if (title === 'webdriver - Google Search') {
       console.log('Test passed')
     } else {
